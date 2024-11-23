52ee2587151db64889211d931938c93231b28807
tetr4lab. <48040768+tetr4lab@users.noreply.github.com>
2024-11-23 14:49:24 +0900

    天気予報クラスの拡充

--- WeatherForecast/Data/WeatherForecast.cs
+++ WeatherForecast/Data/WeatherForecast.cs
@@ -1,19 +1,34 @@
 ﻿namespace WeatherCast.Data;
 
 public class WeatherForecast {
-    public DateOnly Date { get; set; }
-    public int TemperatureC { get; set; }
-    public string? Summary { get; set; }
-    public int TemperatureF => 32 + (int) (TemperatureC / 0.5556);
 
+    protected static readonly int MinTemperature = -20;
+    protected static readonly int MaxTemperature = 55;
+    public DateOnly Date { get; init; }
+    public float TemperatureC { get; init; }
+    public string Summary => Summaries [Math.Min (Summaries.Length - 1, (int) ((TemperatureC - MinTemperature) / (MaxTemperature - MinTemperature) * Summaries.Length))];
+    public float TemperatureF => 32f + (TemperatureC / 0.5556f);
+    protected static string [] Summaries { get; } = { "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching" };
+
+    /// <summary>本日</summary>
+    public WeatherForecast () : this (DateOnly.FromDateTime (DateTime.Now)) { }
+
+    /// <summary>指定日</summary>
+    public WeatherForecast (DateOnly date) {
+        Date = date;
+        TemperatureC = Random.Shared.Next (MinTemperature, MaxTemperature);
+    }
+
+    /// <summary>1日分 遅延付き</summary>
+    public static async Task<WeatherForecast> CreateAsync (DateOnly date) {
+        var forecast = new WeatherForecast (date);
+        await Task.Delay (100);
+        return forecast;
+    }
+
+    /// <summary>5日分 遅延付き</summary>
     public static async Task<WeatherForecast []?> CreateAsync () {
-        var startDate = DateOnly.FromDateTime (DateTime.Now);
-        var summaries = new [] { "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching" };
-        var forecasts = Enumerable.Range (1, 5).Select (index => new WeatherForecast {
-            Date = startDate.AddDays (index),
-            TemperatureC = Random.Shared.Next (-20, 55),
-            Summary = summaries [Random.Shared.Next (summaries.Length)]
-        }).ToArray ();
+        var forecasts = Enumerable.Range (1, 5).Select (index => new WeatherForecast ()).ToArray ();
         await Task.Delay (500);
         return forecasts;
     }
